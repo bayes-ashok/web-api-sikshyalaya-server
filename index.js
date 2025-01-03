ae27cef18635a8d3d1bf4d58f41f49076d11dfbd
Ashok Shah <bayes.ashok@gmail.com>
2025-01-03 22:29:44 +0545

    signup login api created with token

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import express from "express";
 import connectDB from './database/db.js'; 
+import dotenv from "dotenv";
+import userRoute from "./routes/userRoute.js"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config({});
 
 // call database connection here
 connectDB();
 const app = express();
 
-// const PORT = process.env.PORT || 3000;
-const PORT = 8000;
+
+const PORT = process.env.PORT;
+// const PORT = 8000;
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin:"http://localhost:8000",
+    credentials:true
+}));
+
+
+
+app.use("/api/sikshyalaya/user", userRoute);
+
 
 app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}`);
