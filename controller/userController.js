4a4277ff27ee0558ec20f8b0ac7e9796b494431e
Ashok Shah <bayes.ashok@gmail.com>
2025-01-07 15:56:23 +0545

    logout logic created

--- controller/userController.js
+++ controller/userController.js
@@ -73,3 +73,19 @@ export const login = async (req,res) => {
         })
     }
 }
+
+export const logout = async (_,res) => {
+    try {
+        return res.status(200).cookie("token", "", {maxAge:0}).json({
+            message:"Logged out successfully.",
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"Failed to logout"
+        }) 
+    }
+}
+
