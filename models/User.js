6f2b983d0879cf119e5c89b93eff8c8a6b24d57c
Ashok Shah <bayes.ashok@gmail.com>
2025-02-21 20:37:31 +0545

    added phone field as required during payment

--- models/User.js
+++ models/User.js
@@ -5,6 +5,8 @@ const UserSchema = new mongoose.Schema({
   userEmail: String,
   password: String,
   role: String,
+  phone: String,   
+  image: String,
 });
 
 module.exports = mongoose.model("User", UserSchema);
