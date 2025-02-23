6e59228e649476739ae248a6c407784a135dd927
Ashok Shah <bayes.ashok@gmail.com>
2025-02-23 22:57:34 +0545

    update course by id completed

--- controllers/instructor-controller/course-controller.js
+++ controllers/instructor-controller/course-controller.js
@@ -22,6 +22,55 @@ const addNewCourse = async (req, res) => {
   }
 };
 
+const getAllCourses = async (req, res) => {
+    try {
+      const coursesList = await Course.find({});
+  
+      res.status(200).json({
+        success: true,
+        data: coursesList,
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
+        success: false,
+        message: "Some error occured!",
+      });
+    }
+  };
+  
+  
+const updateCourseByID = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updatedCourseData = req.body;
+  
+      const updatedCourse = await Course.findByIdAndUpdate(
+        id,
+        updatedCourseData,
+        { new: true }
+      );
+  
+      if (!updatedCourse) {
+        return res.status(404).json({
+          success: false,
+          message: "Course not found!",
+        });
+      }
+  
+      res.status(200).json({
+        success: true,
+        message: "Course updated successfully",
+        data: updatedCourse,
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
+        success: false,
+        message: "Some error occured!",
+      });
+    }
+  };
 
 module.exports = {
   addNewCourse,
