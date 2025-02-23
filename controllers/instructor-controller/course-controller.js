652e3c3b57f9812ca13d3d490a3cb341122390c2
Ashok Shah <bayes.ashok@gmail.com>
2025-02-23 22:56:46 +0545

    added the add course feature for admin in controller

---
+++ controllers/instructor-controller/course-controller.js
@@ -0,0 +1,31 @@
+const Course = require("../../models/Course");
+
+const addNewCourse = async (req, res) => {
+  try {
+    const courseData = req.body;
+    const newlyCreatedCourse = new Course(courseData);
+    const saveCourse = await newlyCreatedCourse.save();
+
+    if (saveCourse) {
+      res.status(201).json({
+        success: true,
+        message: "Course saved successfully",
+        data: saveCourse,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+
+module.exports = {
+  addNewCourse,
+  getAllCourses,
+  updateCourseByID,
+  getCourseDetailsByID,
+};
