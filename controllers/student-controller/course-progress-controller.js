5ade7d0a4d59931071282e4f99c27fc5f1d8659d
Ashok Shah <bayes.ashok@gmail.com>
2025-02-24 17:34:32 +0545

    reset progress added so that student can watch again if they complete

--- controllers/student-controller/course-progress-controller.js
+++ controllers/student-controller/course-progress-controller.js
@@ -144,3 +144,47 @@ const getCurrentCourseProgress = async (req, res) => {
     });
   }
 };
+
+
+
+//reset course progress
+
+const resetCurrentCourseProgress = async (req, res) => {
+    try {
+      const { userId, courseId } = req.body;
+  
+      const progress = await CourseProgress.findOne({ userId, courseId });
+  
+      if (!progress) {
+        return res.status(404).json({
+          success: false,
+          message: "Progress not found!",
+        });
+      }
+  
+      progress.lecturesProgress = [];
+      progress.completed = false;
+      progress.completionDate = null;
+  
+      await progress.save();
+  
+      res.status(200).json({
+        success: true,
+        message: "Course progress has been reset",
+        data: progress,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: "Some error occured!",
+      });
+    }
+  };
+  
+  module.exports = {
+    markCurrentLectureAsViewed,
+    getCurrentCourseProgress,
+    resetCurrentCourseProgress,
+  };
+  
